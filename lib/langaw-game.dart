5850c9a00f2e915dbebe6ab755a2763129dff4b8
t0pth4t <dev@noliver.io>
2020-02-14 21:13:53 -0600

    graphics

--- lib/langaw-game.dart
+++ lib/langaw-game.dart
@@ -2,8 +2,14 @@ import 'dart:math';
 import 'dart:ui';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:langaw/components/agile-fly.dart';
+import 'package:langaw/components/backyard.dart';
+import 'package:langaw/components/drooler-fly.dart';
 import 'package:langaw/components/fly.dart';
 import 'package:flutter/gestures.dart';
+import 'package:langaw/components/house-fly.dart';
+import 'package:langaw/components/hungry-fly.dart';
+import 'package:langaw/components/macho-fly.dart';
 
 class LangawGame extends Game {
   Size screenSize;
@@ -11,14 +17,18 @@ class LangawGame extends Game {
   List<Fly> flies;
   Random rnd;
   int fliesToAdd = 0;
+  Backyard background;
+
   LangawGame(){
     init();
   }
 
   void init() async {
+
     flies = List<Fly>();
     rnd = Random();
     resize(await Flame.util.initialDimensions());
+    background = Backyard(this);
     spawnFly();
   }
 
@@ -26,14 +36,28 @@ class LangawGame extends Game {
   void spawnFly() {
     double x = rnd.nextDouble() * (screenSize.width - tileSize);
     double y = rnd.nextDouble() * (screenSize.height - tileSize);
-    flies.add(Fly(this, x, y));
+    switch (rnd.nextInt(5)) {
+      case 0:
+        flies.add(HouseFly(this, x, y));
+        break;
+      case 1:
+        flies.add(DroolerFly(this, x, y));
+        break;
+      case 2:
+        flies.add(AgileFly(this, x, y));
+        break;
+      case 3:
+        flies.add(MachoFly(this, x, y));
+        break;
+      case 4:
+        flies.add(HungryFly(this, x, y));
+        break;
+    }
   }
 
   void render(Canvas canvas) {
-    Rect bgRect = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
-    Paint bgPaint = Paint();
-    bgPaint.color = Color(0xff576574);
-    canvas.drawRect(bgRect, bgPaint);
+    background.render(canvas);
+
     if(flies != null){
       flies.forEach((Fly fly) => fly.render(canvas));
     }
